42ded28a03621fde469d076bb385cd29df38975c
Scott Lewis-Kelly <slewiskelly@mercari.com>
2022-01-06 14:51:10 +1100

    internal/deps: Fix imports
    Syntax error.

--- internal/deps/deps.go
+++ internal/deps/deps.go
@@ -1,7 +1,9 @@
 // Package deps declares a set of packages from which CUE definitions are
 // generated.
 
+//go:build deps
 // +build deps
+
 package deps
 
 import (
@@ -18,4 +20,4 @@ import (
 	_ "k8s.io/autoscaler/vertical-pod-autoscaler/pkg/apis/autoscaling.k8s.io/v1"
 	_ "k8s.io/ingress-gce/pkg/apis/backendconfig/v1"
 	_ "k8s.io/ingress-gce/pkg/apis/frontendconfig/v1beta1"
-}
+)
